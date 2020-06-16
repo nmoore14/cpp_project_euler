544b34ace65ceed4cb2ef0a99cc857c91be42d44
Nick Moore <nickmoore_1@live.com>
2020-06-15 22:25:35 -0400

    Finishing problem 1

--- problem_1/problem_1.cpp
+++ problem_1/problem_1.cpp
@@ -3,4 +3,26 @@
  *  Problem 1
  *  Find the sum of all of the multiples of 3 or 5 below 100
  *
+ *  Answer: 233168
+ *
  * */
+
+#include <iostream>
+
+int main() {
+  int sum = 0;
+  int n = 0;
+
+  while (n < 1000) {
+    if (n % 3 == 0 || n % 5 == 0) {
+      sum += n;
+      n ++;
+    } else {
+      n ++;
+    }
+  }
+
+  std::cout << "Total: " << sum << std::endl;
+
+  return 0;
+}
